4736e561088028ed43ddf3d749f36f2e90e5d655
SirajMia <sirajulshuvo08@gmail.com>
2024-05-05 15:41:38 +0600

    add hero section

---
+++ js/index.js
@@ -0,0 +1,29 @@
+// Link hover effect
+function linkHover(){
+    const navLink = Array.from(document.querySelectorAll('#navlink'));
+    // console.log(navLink)
+    navLink.forEach((element) => {
+        element.addEventListener('mouseover',(e) =>{
+            element.classList.add('navactive');
+        })
+        element.addEventListener('mouseleave',(e) =>{
+            element.classList.remove('navactive');
+        })
+    })
+}
+linkHover();
+
+// Respoinsive menubar
+function responsiveHeader(){
+    const menuIcon = document.querySelector('.menu-icon');
+    const header = document.querySelector('header');
+
+    menuIcon.addEventListener('click',(e) => {
+        header.classList.add('activeheader');
+    })
+    menuIcon.addEventListener('dblclick',(e) => {
+        header.classList.remove('activeheader');
+        header.style.transition = '0.6s all linear';
+    })
+}
+responsiveHeader();
